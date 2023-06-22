b2a7aa9ec4d52467cd985c6c92549495defa9a58
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-06-22 15:33:06 +0300

    Load all images

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,5 +2,9 @@ import React, { useState } from "react";
 import importAll from "../utils/importAll";
 
 export default function Cards() {
+  const photos = importAll(
+    require.context("../assets/", false, /\.(png|jpe?g|svg|webp)$/)
+  );
+  console.log(photos);
   return <div></div>;
 }
