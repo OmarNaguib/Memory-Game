e60d4566130379c1aadd8d390fead68da428d6c4
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-06-22 17:30:03 +0300

    Layout and markup cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,7 +5,14 @@ export default function Cards() {
   const photos = importAll(
     require.context("../assets/", false, /\.(png|jpe?g|svg|webp)$/)
   );
-
+  const cards = photos.map((item) => {
+    return (
+      <div className="card">
+        <img src={item.src} alt={item.name} />
+        <h2>{item.name}</h2>
+      </div>
+    );
+  });
   console.log(photos);
-  return <div></div>;
+  return <div className="cards">{cards}</div>;
 }
